e2efaf64e16bc358c8ee37861980cd8f7883b9b1
Gabriel Rodrigues <gr@gapp.im>
2018-07-02 02:10:59 -0300

    Updating exp2.

--- scalability/exp2/plan_size_vs_time.r
+++ scalability/exp2/plan_size_vs_time.r
@@ -1,5 +1,32 @@
-data <- read.csv(file="plan_size_vs_time.dat" ,head=TRUE,sep="\t")
-setEPS()
-postscript("plan_size_vs_time.eps")
-boxplot(Z~X,data=data, xlab="Size", ylab="Time(ms)")
+#install.packages("dplyr")
+# library(ggplot2)
+library(dplyr)
+
+dataframe <- read.csv(file="scalability_over_plan_size.dat" ,head=TRUE,sep="\t")
+
+initial_deployment_dam_update <- tbl_df(dataframe) %>% 
+  group_by(bundles_count, numberOfGoals, variability) %>%
+  summarise(avg_time = mean( (dam_updated_0+ deployment_change_planned_0)/1000000),  std=sd( (dam_updated_0 + deployment_change_planned_2) /1000000))
+
+pdf("initial_deployment_dam_update.pdf")
+boxplot(avg_time~bundles_count,data=initial_deployment_dam_update, xlab="Size", ylab="Time(ms)")
 dev.off()
+
+dam_updated_1 <- tbl_df(dataframe) %>% 
+  group_by(bundles_count, numberOfGoals, variability) %>%
+  summarise(avg_time = mean( (dam_updated_1 + deployment_change_planned_1) /1000000),  std=sd( (dam_updated_1+ deployment_change_planned_1)/1000000))
+
+pdf("dam_updated_1.pdf")
+boxplot(avg_time~bundles_count,data=dam_updated_1, xlab="Size", ylab="Time(ms)")
+dev.off()
+
+dam_updated_2 <- tbl_df(dataframe) %>% 
+  group_by(bundles_count, numberOfGoals, variability) %>%
+  summarise(avg_time = mean( (dam_updated_2 + deployment_change_planned_2) /1000000),  std=sd( (dam_updated_2 + deployment_change_planned_2)/1000000))
+
+#data <- read.csv(file="plan_size_vs_time.dat" ,head=TRUE,sep="\t")
+#setPDF()
+pdf("dam_updated_2.pdf")
+boxplot(avg_time~bundles_count,data=dam_updated_2, xlab="Size", ylab="Time(ms)")
+dev.off()
+
